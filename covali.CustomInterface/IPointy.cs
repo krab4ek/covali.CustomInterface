aaef7f4502bd6015c855219bb88f3f62a6de6c17
Covali Andrei <krab4ek@gmail.com>
2022-07-13 21:48:34 +0300

    Add project files.

---
+++ covali.CustomInterface/IPointy.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace covali.CustomInterface
+{
+    byte Points { get; }
+    internal interface IPointy
+    {
+        byte GetNumberOfPoints();
+    }
+}
